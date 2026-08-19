dd35af3d155a72bdbf78b5453b634acb306f72ae
yasmeenafayz-jpg <yasmeenafayz@gmail.com>
2026-08-19 15:00:06 +0300

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import "./styles/app.css";
+import { App } from "./app.js";
+
+const root = document.querySelector("#app");
+
+if (!root) {
+  throw new Error("APP_ROOT_NOT_FOUND");
+}
+
+const application = new App(root);
+
+application.mount();
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").catch(() => {});
+  });
+}
